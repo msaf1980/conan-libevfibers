e2167294d5e46fa40731bfdb2951b17bac928654
Safronov Michail <msaf1980@gmail.com>
2019-10-01 14:10:21 +0500

    0.4.1-58-ga8a5b81

--- conanfile.py
+++ conanfile.py
@@ -5,12 +5,15 @@ from conans import CMake, ConanFile, AutoToolsBuildEnvironment, tools
 
 class CxxoptsConan(ConanFile):
     name = "libevfibers"
+    libname = "evfibers"
     branch = "master"
-    scn = "8f0a41927bf8c1b8a9c5a3adb254247303b50b17"
-    version = "0.4.1-57-g8f0a419"
+    #scn = "8f0a41927bf8c1b8a9c5a3adb254247303b50b17"
+    scn = "a8a5b81698f0c7f343745dcdbfcb0f6c6fe4ff32"
+    version = "0.4.1-58-ga8a5b81"
     license = "MIT"
     author = "Darlan Cavalcante Moreira (darcamo@gmail.com)"
-    url = "https://github.com/Lupus/libevfibers"
+    #url = "https://github.com/Lupus/libevfibers"
+    url = "https://github.com/msaf1980/libevfibers"
     description = "A small C fiber library that uses libev based event loop and libcoro based coroutine context switching"
     homepage = "http://olkhovskiy.me/libevfibers/"
 
@@ -19,6 +22,7 @@ class CxxoptsConan(ConanFile):
     #exports_sources = "%/src/*" % name
 
     def source(self):
+        print("git clone %s -b %s" % (self.url, self.branch))
         self.run("git clone %s -b %s" % (self.url, self.branch))
         self.run("cd %s && git reset --hard %s" % (self.name, self.scn))
 
@@ -28,7 +32,10 @@ class CxxoptsConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("include/%s/*.h" % self.name, dst="include")
+        include = "include/%s" % self.libname
+        src_include = "%s/%s" % (self.libname, self.name)
+        self.copy("*.h", src=include, dst=include)
+        self.copy("*.h", src="libevfibers/include/evfibers", dst=include)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
@@ -36,4 +43,4 @@ class CxxoptsConan(ConanFile):
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["evfibers"]
+        self.cpp_info.libs = [ self.libname ]
